8adb4a9b694b1cbbdbce13bb16c2a4f794183f7a
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-11-30 19:03:49 -0600

    AddingDatabaseAndUsers

---
+++ Facebook.js
@@ -0,0 +1,13 @@
+var database = [
+    {
+        username: "andrei",
+        password: "supersecret"
+    }
+];
+
+var newsfeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all this learning!"
+    },
+];
