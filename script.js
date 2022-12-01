17aa4e345087c1c7607902fcba706a9211555e0b
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-11-30 20:07:29 -0600

    finishingProject

--- script.js
+++ script.js
@@ -28,24 +28,24 @@ var newsfeed = [
     }
 ];
 
-function signIn(username, password) {
-    for (var i = 0;  i < database.length; i++) {
+function isUserValid(username, password) {
+    for (var i=0;  i < database.length; i++) {
         if(database[i].username === username && 
             database[i].password  === password) {
-                console.log(newsfeed);
-            } else {
-                alert("Sorry, wrong username and password!");
+                return true;
             }
+        }
+        return false;
+}
 
+
+
+function signIn(username, password) {
+     if (isUserValid(username, password)) {
+         console.log(newsfeed);
+     } else {
+         alert("Sorry, wrong username and password!");
     }
-
-
-    // if (username === database[0].username 
-    //     && password === database[0].password) {
-    //         console.log(newsfeed);
-    //     } else {
-    //         alert("Sorry, wrong username and password!");
-    //     }
     }
 
 var userNamePrompt = prompt("What\'s your username?");
