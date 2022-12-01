b45c55d512dee59e98f6abe40a8c568d49535253
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-11-30 19:48:18 -0600

    UpdatingCode
    Adding a for loop

--- script.js
+++ script.js
@@ -29,12 +29,23 @@ var newsfeed = [
 ];
 
 function signIn(username, password) {
-    if (username === database[0].username 
-        && password === database[0].password) {
-            console.log(newsfeed);
-        } else {
-            alert("Sorry, wrong username and password!");
-        }
+    for (var i = 0;  i < database.length; i++) {
+        if(database[i].username === username && 
+            database[i].password  === password) {
+                console.log(newsfeed);
+            } else {
+                alert("Sorry, wrong username and password!");
+            }
+
+    }
+
+
+    // if (username === database[0].username 
+    //     && password === database[0].password) {
+    //         console.log(newsfeed);
+    //     } else {
+    //         alert("Sorry, wrong username and password!");
+    //     }
     }
 
 var userNamePrompt = prompt("What\'s your username?");
