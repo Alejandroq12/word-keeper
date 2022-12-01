6d2cc800e3b7881068bd30b1bd1b9d2cd4b02080
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-11-30 19:05:55 -0600

    AddingNewsfeed

--- Facebook.js
+++ Facebook.js
@@ -10,4 +10,12 @@ var newsfeed = [
         username: "Bobby",
         timeline: "So tired from all this learning!"
     },
+    {
+        username: "Sally",
+        timeline: "Javascript is sooooo cool!"
+    },
+    {
+        username: "Mitch",
+        timeline: "Javascript is preeetyy cool!"
+    }
 ];
