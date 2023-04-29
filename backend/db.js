cecbb6689aced6b1b63dc8d3b259f60959453245
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-04-29 14:09:20 -0600

    Add db.js

---
+++ backend/db.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
